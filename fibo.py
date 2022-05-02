8c0bbc83bd63dda752c7ea72fabcccd6adc212bc
Tukanka <teru.kalivodova@gmail.com>
2022-05-02 09:21:54 +0200

    Fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
 
 def main():
-    pass
+    count = int(input('How many numbers?'))
+    fib_seq = []
+    for n in range(count):
+        fib_seq.append(recursive_nth_fibo(n))
+    print(fib_seq)
 
 
 if __name__ == "__main__":
